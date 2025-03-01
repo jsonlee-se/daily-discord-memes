5b0aab10aa8c396feda88bc0b0e951f67f367bdb
Jayson-Lee Gillis <jayson-lee.gillis@hotmail.be>
2025-03-01 20:14:22 +0100

    refactor(app): codelines split over multiple lines.. (these changes are over a year old, i'm now just pushing them)

--- app.py
+++ app.py
@@ -6,18 +6,30 @@ from discord.ext import commands
 
 import modal
 
-app = modal.App(name="daily_memes",image=modal.Image.debian_slim().pip_install("discord.py", "requests"))
+app = modal.App(
+    name="daily_memes",
+    image=modal.Image.debian_slim().pip_install("discord.py", "requests"),
+)
 
 intents = discord.Intents.all()
 intents.message_content = True
 client = commands.Bot(command_prefix="/", intents=intents)
-subreddits = ["Dankmemes", "okbuddyretard", "Animemes", "clevercomebacks", "meirl", "dndmemes","facepalm"]
+subreddits = [
+    "Dankmemes",
+    "okbuddyretard",
+    "Animemes",
+    "clevercomebacks",
+    "meirl",
+    "dndmemes",
+    "facepalm",
+]
+
 
 @client.event
 async def on_ready(secret=modal.Secret.from_name("daily-discord-memes")):
-    print(f'Logged in as {client.user.name}')
+    print(f"Logged in as {client.user.name}")
     CHANNEL_ID = int(os.environ["CHANNEL_ID"])
-    
+
     for sub in subreddits:
         response = make_request(subreddit=sub)
         try:
@@ -32,6 +44,7 @@ async def on_ready(secret=modal.Secret.from_name("daily-discord-memes")):
 
     await client.close()
 
+
 def make_request(subreddit, secret=modal.Secret.from_name("daily-discord-memes")):
     REDDIT_CLIENT_ID = os.environ["REDDIT_CLIENT_ID"]
     REDDIT_CLIENT_SECRET = os.environ["REDDIT_CLIENT_SECRET"]
@@ -39,17 +52,24 @@ def make_request(subreddit, secret=modal.Secret.from_name("daily-discord-memes")
     REDDIT_PASSWORD = os.environ["REDDIT_PASSWORD"]
 
     client_auth = requests.auth.HTTPBasicAuth(REDDIT_CLIENT_ID, REDDIT_CLIENT_SECRET)
-    post_data = {"grant_type": "password", "username": REDDIT_USERNAME, "password": REDDIT_PASSWORD}
+    post_data = {
+        "grant_type": "password",
+        "username": REDDIT_USERNAME,
+        "password": REDDIT_PASSWORD,
+    }
     headers = {"User-Agent": "DM by someguy"}
-    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+    response = requests.post(
+        "https://www.reddit.com/api/v1/access_token",
+        auth=client_auth,
+        data=post_data,
+        headers=headers,
+    )
     response.json()
-    bearer_token = response.json()['access_token']
+    bearer_token = response.json()["access_token"]
 
     url = "https://oauth.reddit.com//r/" + subreddit + "/top.json?limit=1"
 
-    headers = {"Authorization": "bearer " + bearer_token, 
-            "User-Agent": "DM by someguy"
-            }
+    headers = {"Authorization": "bearer " + bearer_token, "User-Agent": "DM by someguy"}
     response = requests.request("GET", url, headers=headers)
 
     if response.status_code == 200:
@@ -63,7 +83,11 @@ def make_request(subreddit, secret=modal.Secret.from_name("daily-discord-memes")
         print(response.text)
         return None
 
-@app.function(secrets=[modal.Secret.from_name("daily-discord-memes")],schedule=modal.Period(days=1))
+
+@app.function(
+    secrets=[modal.Secret.from_name("daily-discord-memes")],
+    schedule=modal.Period(days=1),
+)
 def run_bot():
     client.run(os.environ["DISCORD_BOT_TOKEN"])
 
@@ -71,3 +95,4 @@ def run_bot():
 @app.local_entrypoint()
 def main():
     run_bot.remote()
+
